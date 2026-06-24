2278787b800658b0afe96437cbebadd8a9d482a1
keerthigan-teceze <Keerthigan.Thevarasa@teceze.com>
2026-06-24 15:48:35 +0530

    db configuration done with prisma

--- prisma/migrations/20260624101542_init/migration.sql
+++ prisma/migrations/20260624101542_init/migration.sql
@@ -1,11 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Session";
+
 -- CreateTable
-CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+CREATE TABLE "Session_de" (
+    "id" TEXT NOT NULL,
     "shop" TEXT NOT NULL,
     "state" TEXT NOT NULL,
     "isOnline" BOOLEAN NOT NULL DEFAULT false,
     "scope" TEXT,
-    "expires" TIMESTAMP,
+    "expires" TIMESTAMP(6),
     "accessToken" TEXT NOT NULL,
     "userId" BIGINT,
     "firstName" TEXT,
@@ -16,5 +25,10 @@ CREATE TABLE "Session" (
     "collaborator" BOOLEAN DEFAULT false,
     "emailVerified" BOOLEAN DEFAULT false,
     "refreshToken" TEXT,
-    "refreshTokenExpires" TIMESTAMP
+    "refreshTokenExpires" TIMESTAMP(6),
+
+    CONSTRAINT "Session_de_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_de_shop_key" ON "Session_de"("shop");
